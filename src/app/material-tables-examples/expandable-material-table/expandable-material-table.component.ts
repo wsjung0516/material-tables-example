23fdfc875e4b118e4daca8138c99a24a0f25e653
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2019-08-15 07:04:26 +0200

    add expandable material table example

---
+++ src/app/material-tables-examples/expandable-material-table/expandable-material-table.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { DataSource } from '@angular/cdk/table';
+import { Observable, of } from 'rxjs';
+
+export class ExampleDataSource extends DataSource<any> {
+
+  data: any[] = [];
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Object[]> {
+    const rows = [];
+    this.data.forEach(element => rows.push(element, { detailRow: true, element }));
+    return of(rows);
+  }
+
+  disconnect() { }
+}
+
+@Component({
+  selector: 'app-expandable-material-table',
+  templateUrl: './expandable-material-table.component.html',
+  styleUrls: ['./expandable-material-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class ExpandableMaterialTableComponent implements OnInit {
+  displayedColumns = ['name', 'surname', 'age'];
+
+  data =
+  [
+    {
+      name: 'Grzegorz',
+      surname: 'Brzęczyszczykiewicz',
+      age: 22
+    },
+    {
+      name: 'Marian',
+      surname: 'Paździoch',
+      age: 60
+    },
+    {
+      name: 'Waldemar',
+      surname: 'Kiepski',
+      age: 35
+    }
+  ];
+
+  expandedElement: any;
+
+  dataSource = new ExampleDataSource();
+
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
+
+  constructor() {
+    this.dataSource.data = this.data;
+  }
+
+  ngOnInit() {
+  }
+
+}
+
