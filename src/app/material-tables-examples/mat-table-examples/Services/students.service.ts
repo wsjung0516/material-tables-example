12a0a4389fa78ae5de3f8fe7b982d6b48d018226
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2019-09-19 05:25:36 +0200

    Added some more date for studenst serivice table

--- src/app/material-tables-examples/mat-table-examples/Services/students.service.ts
+++ src/app/material-tables-examples/mat-table-examples/Services/students.service.ts
@@ -25,6 +25,78 @@ export class StudentsService {
       name: 'Waldemar',
       surname: 'Kiepski',
       age: 35
+    },
+    {
+      id: 4,
+      name: 'Sebastian',
+      surname: 'Stępień',
+      age: 20
+    },
+    {
+      id: 5,
+      name: 'Kacper',
+      surname: 'Krawczyk',
+      age: 20
+    },
+    {
+      id: 6,
+      name: 'Maja',
+      surname: 'Pawłowska',
+      age: 20
+    },
+    {
+      id: 7,
+      name: 'Aleksandra',
+      surname: 'Nowak',
+      age: 20
+    },
+    {
+      id: 8,
+      name: 'Oliwier',
+      surname: 'Andrzejewski',
+      age: 20
+    },
+    {
+      id: 9,
+      name: 'Jakub',
+      surname: 'Czech',
+      age: 20
+    },
+    {
+      id: 10,
+      name: 'Antonina',
+      surname: 'Tomaszewska',
+      age: 20
+    },
+    {
+      id: 11,
+      name: 'Antonina',
+      surname: 'Olszewska',
+      age: 20
+    },
+    {
+      id: 12,
+      name: 'Patrycja',
+      surname: 'Augustyniak',
+      age: 20
+    },
+    {
+      id: 13,
+      name: 'Kajetan',
+      surname: 'Tomaszewski',
+      age: 20
+    },
+    {
+      id: 14,
+      name: 'Aleksandra',
+      surname: 'Kowalska',
+      age: 20
+    },
+    {
+      id: 15,
+      name: 'Michał',
+      surname: 'Dąbrowski',
+      age: 20
     }
   ];
 
